4fd441bbebc153ebf0893c4624dfb3f5d2bce6a2
SahitiReddyKeesari <34088582+SahitiReddyKeesari@users.noreply.github.com>
2021-12-16 15:24:45 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@assets/main.css'
 import '@assets/chrome-bug.css'
 import 'keen-slider/keen-slider.min.css'
-
+import Script from 'next/script'
 import { FC, useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import { Head } from '@components/common'
@@ -24,6 +24,35 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </Layout>
       </ManagedUIContext>
+       <Script
+
+dangerouslySetInnerHTML={{
+
+  __html: `
+
+  window["adrum-start-time"] = new Date().getTime();
+  (function(config){
+      config.appKey = "AD-AAB-ABJ-ZXM";
+      config.adrumExtUrlHttp = "http://cdn.appdynamics.com";
+      config.adrumExtUrlHttps = "https://cdn.appdynamics.com";
+      config.beaconUrlHttp = "http://pdx-col.eum-appdynamics.com";
+      config.beaconUrlHttps = "https://pdx-col.eum-appdynamics.com";
+      config.useHTTPSAlways = true;
+      config.resTiming = {"bufSize":200,"clearResTimingOnBeaconSend":true};
+      config.maxUrlLength = 512;
+      config.spa = {"spa2":true};
+  })(window["adrum-config"] || (window["adrum-config"] = {}));'
+
+
+  `
+
+}}
+
+/>
+
+<Script src="//cdn.appdynamics.com/adrum/adrum-21.7.0.3493.js" />
+   
+      
     </>
   )
 }
